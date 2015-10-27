36c4d4179294a7695a7f7f231c24b7fb7d2361c0
MarcDuboc <marc.duboc@cifacom.com>
2015-10-27 11:23:13 +0100

    re add settings

---
+++ sites/default/files/php/twig/fa6d0021_block.html.twig_9a17b816786b694f928d9c28ef99486949b747935fbe5c8c8bdb37d365482cbe/1c4ff680afb09587e04ed09ecc8282c902bee75c5d52937b0a331ba372b08a8f.php
@@ -0,0 +1,139 @@
+<?php
+
+/* @block/block.html.twig */
+class __TwigTemplate_373ec56078982076df2252c5055571911e8deb57372610714ef8176e5bee3926 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 33, "block" => 37);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('if', 'block'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 31
+        echo "<div";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">
+  ";
+        // line 32
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_prefix"]) ? $context["title_prefix"] : null), "html", null, true));
+        echo "
+  ";
+        // line 33
+        if ((isset($context["label"]) ? $context["label"] : null)) {
+            // line 34
+            echo "    <h2";
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_attributes"]) ? $context["title_attributes"] : null), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["label"]) ? $context["label"] : null), "html", null, true));
+            echo "</h2>
+  ";
+        }
+        // line 36
+        echo "  ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_suffix"]) ? $context["title_suffix"] : null), "html", null, true));
+        echo "
+  ";
+        // line 37
+        $this->displayBlock('content', $context, $blocks);
+        // line 40
+        echo "</div>
+";
+    }
+
+    // line 37
+    public function block_content($context, array $blocks = array())
+    {
+        // line 38
+        echo "    ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "@block/block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  78 => 38,  75 => 37,  70 => 40,  68 => 37,  63 => 36,  55 => 34,  53 => 33,  49 => 32,  44 => 31,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Default theme implementation to display a block.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - plugin_id: The ID of the block implementation.*/
+/*  * - label: The configured label of the block if visible.*/
+/*  * - configuration: A list of the block's configuration values.*/
+/*  *   - label: The configured label for the block.*/
+/*  *   - label_display: The display settings for the label.*/
+/*  *   - module: The module that provided this block plugin.*/
+/*  *   - cache: The cache settings.*/
+/*  *   - Block plugin specific settings will also be stored here.*/
+/*  * - content: The content of this block.*/
+/*  * - attributes: array of HTML attributes populated by modules, intended to*/
+/*  *   be added to the main container tag of this template.*/
+/*  *   - id: A valid HTML ID and guaranteed unique.*/
+/*  * - title_attributes: Same as attributes, except applied to the main title*/
+/*  *   tag that appears in the template.*/
+/*  * - title_prefix: Additional output populated by modules, intended to be*/
+/*  *   displayed in front of the main title tag that appears in the template.*/
+/*  * - title_suffix: Additional output populated by modules, intended to be*/
+/*  *   displayed after the main title tag that appears in the template.*/
+/*  **/
+/*  * @see template_preprocess_block()*/
+/*  **/
+/*  * @ingroup themeable*/
+/*  *//* */
+/* #}*/
+/* <div{{ attributes }}>*/
+/*   {{ title_prefix }}*/
+/*   {% if label %}*/
+/*     <h2{{ title_attributes }}>{{ label }}</h2>*/
+/*   {% endif %}*/
+/*   {{ title_suffix }}*/
+/*   {% block content %}*/
+/*     {{ content }}*/
+/*   {% endblock %}*/
+/* </div>*/
+/* */
